31c798e308fc3d318f34112673ae5975c0b2e5a8
amanoj319319319 <a.manoj16@gmail.com>
2020-10-23 18:13:49 +0530

    some statements were added

--- Basics/Trignometry.py
+++ Basics/Trignometry.py
@@ -1,3 +1,6 @@
 print ("Hi boss .. From tomorrow onwards trignometry related concepts will be started  ...")
 print ("hi guys .. after listening first session of trignometry , how did you feel about the lecture ?")
 print ("Hi java ... how are you ...")
+print ("python is easy ")
+print ("java is also easy language ...")
+print ("database is easy concept ")
