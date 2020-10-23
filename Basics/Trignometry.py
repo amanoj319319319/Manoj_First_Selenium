4583583331e6f3253250fdbb1be4502db28ba5af
amanoj319319319 <a.manoj16@gmail.com>
2020-10-23 17:18:29 +0530

    I added few statements in the trognometry file

--- Basics/Trignometry.py
+++ Basics/Trignometry.py
@@ -1,1 +1,2 @@
 print ("Hi boss .. From tomorrow onwards trignometry related concepts will be started  ...")
+print ("hi guys .. after listening first session of trignometry , how did you feel about the lecture ?")
